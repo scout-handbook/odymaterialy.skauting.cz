cde1a7736cb0321cefd3f74249a9b951cd0c91e5
GenaBitu <genabitu@gmail.com>
2017-02-15 19:11:11 +0100

    Separate selectLesson function

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-function xhttpPost()
-{
-	if (this.readyState == 4 && this.status == 200)
-	{
-		var converter = new showdown.Converter();
-		var html = converter.makeHtml(this.responseText);
-		document.getElementById("main_id").innerHTML = html;
-	}
-}
-function run()
+function selectLesson(lesson)
 {
 	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = xhttpPost;
+	xhttp.onreadystatechange = function()
+	{
+		if (this.readyState == 4 && this.status == 200)
+		{
+			var converter = new showdown.Converter();
+			var html = converter.makeHtml(this.responseText);
+			document.getElementById("main_id").innerHTML = html;
+		}
+	}
 	xhttp.open("POST", "API/get_lesson.php", true);
 	xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	xhttp.send("name=Prostředky");
+	xhttp.send("name=" + lesson);
+}
+
+function run()
+{
+	selectLesson("Motivace k činnosti v Junáku");
 }
 
 window.onload = run;
