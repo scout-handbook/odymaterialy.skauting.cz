6e5464a4dc7a118fa6153e74c9288c601f68ade4
GenaBitu <genabitu@gmail.com>
2017-02-15 18:03:14 +0100

    Basic Lesson rendering

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
+function xhttpPost()
+{
+	if (this.readyState == 4 && this.status == 200)
+	{
+		var converter = new showdown.Converter();
+		var html = converter.makeHtml(this.responseText);
+		document.getElementById("main_id").innerHTML = html;
+	}
+}
 function run()
 {
-	var converter = new showdown.Converter();
-	var src = "#Hello, markdown!";
-	//var html = converter.makeHtml(src);
 	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function()
-	{
-		if (this.readyState == 4 && this.status == 200)
-		{
-			document.getElementById("main_id").innerHTML = this.responseText;
-		}
-	};
-	  xhttp.open("GET", "API/list_lessons.php", true);
-	  xhttp.send();
-	//document.getElementById("main_id").innerHTML = html;
+	xhttp.onreadystatechange = xhttpPost;
+	xhttp.open("POST", "API/get_lesson.php", true);
+	xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	xhttp.send("name=Prostředky");
 }
 
 window.onload = run;
