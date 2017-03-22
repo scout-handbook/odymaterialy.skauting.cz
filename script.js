bcf1726402a0ca08829e3c8508bc7a1bf13b5206
GenaBitu <genabitu@gmail.com>
2017-03-23 00:20:06 +0100

    On handheld added content overlay on nav open with clickaction closing the nav

--- script.js
+++ script.js
@@ -190,16 +190,19 @@ function reflow()
 		if(screen.width > 700)
 		{
 			document.getElementById("main").style.marginLeft = "300px"
+			document.getElementById("overlay").style.display = "none";
 		}
 		else
 		{
 			document.getElementById("main").style.marginLeft = "0px"
+			document.getElementById("overlay").style.display = "inline";
 		}
 	}
 	else
 	{
 		document.getElementById("navBar").style.marginLeft = "-300px"
 		document.getElementById("main").style.marginLeft = "0px"
+		document.getElementById("overlay").style.display = "none";
 	}
 }
 
