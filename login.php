ea7bd2e9caf24f780ed1c40bff4ec2b33afb5403
GenaBitu <genabitu@gmail.com>
2017-04-11 16:38:46 +0200

    Proper login & logout redirect scripts

--- login.php
+++ login.php
@@ -1,4 +1,26 @@
 <?php
-$url = "/";
-header("Location: $url", TRUE, 307)
+function startsWith($haystack, $needle)
+{
+	$length = strlen($needle);
+	return (substr($haystack, 0, $length) === $needle);
+}
+session_start();
+
+$redirect = $_GET['ReturnUrl'];
+if(startsWith($redirect, 'http://'))
+{
+	$redirect = 'https://' . substr($redirect, 7);
+}
+elseif(!startsWith($redirect, 'https://'))
+{
+	$redirect = 'https://odymaterialy.skauting.cz/' . $redirect;
+}
+
+$token = $_POST['skautIS_Token'];
+$timeout = DateTime::createFromFormat('j. n. Y H:i:s', $_POST['skautIS_DateLogout'])->format('U');
+$_SESSION['skautis_token'] = $token;
+$_SESSION['skautis_timeout'] = $timeout;
+
+header('Location: ' . $redirect);
+die();
 ?>
--- logout.php
+++ logout.php
@@ -1,4 +1,24 @@
 <?php
-$url = "/";
-header("Location: $url", TRUE, 307)
+function startsWith($haystack, $needle)
+{
+	$length = strlen($needle);
+	return (substr($haystack, 0, $length) === $needle);
+}
+session_start();
+
+$redirect = $_GET['ReturnUrl'];
+if(startsWith($redirect, 'http://'))
+{
+	$redirect = 'https://' . substr($redirect, 7);
+}
+elseif(!startsWith($redirect, 'https://'))
+{
+	$redirect = 'https://odymaterialy.skauting.cz/' . $redirect;
+}
+
+session_unset();
+session_destroy();
+
+header('Location: ' . $redirect);
+die();
 ?>
