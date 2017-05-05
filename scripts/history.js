94ebd1fb00d5e91066748f273d6327f22baa2970
GenaBitu <genabitu@gmail.com>
2017-05-05 10:37:15 +0200

    Simplified history popback

--- scripts/history.js
+++ scripts/history.js
@@ -10,14 +10,7 @@ function historySetup()
 
 function popback()
 {
-	if (window.location.pathname.substring(0, 8) === "/lesson/")
-	{
-		var lessonName = decodeURIComponent(window.location.pathname.substring(8));
-		getLesson(lessonName, true);
-	}
-	else
-	{
-		getLesson(undefined, true);
-	}
+	var lessonName = decodeURIComponent(window.location.pathname.substring(8));
+	getLesson(lessonName, true);
 }
 
