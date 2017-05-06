285e6385ea62196b2a793760fe3f6ea3b84448ef
GenaBitu <genabitu@gmail.com>
2017-05-06 21:16:25 +0200

    Fixed logout

--- auth/logout_redirect.php
+++ auth/logout_redirect.php
@@ -1,7 +1,7 @@
 <?php
 
-setcookie('skautis_token', "", time() - 3600);
-setcookie('skautis_timeout', "", time() - 3600);
+setcookie('skautis_token', "", time() - 3600, "/", "odymaterialy.skauting.cz", true, true);
+setcookie('skautis_timeout', "", time() - 3600, "/", "odymaterialy.skauting.cz", true, true);
 unset($_COOKIE['skautis_token']);
 unset($_COOKIE['skautis_timeout']);
 
