98c9c928ba087fb7b74cfea096f1f67f3799288a
GenaBitu <genabitu@gmail.com>
2017-05-06 10:23:13 +0200

    live preview

--- admin/scripts/getLesson.js
+++ admin/scripts/getLesson.js
@@ -23,11 +23,9 @@ function getLesson(lesson, noHistory)
 function showLesson(name, markdown, noHistory)
 {
 	var html = "<header><div id=\"discard\"><i class=\"icon-left-big\"></i>Zrušit</div><div id=\"save\">Uložit<i class=\"icon-floppy\"></i></div></header>"
-	html += "<div id=\"editor\">" + markdown + "</div><div id=\"preview\">";
-	html += "<h1>" + name + "</h1>";
-	html += converter.makeHtml(markdown);
-	html += "</div>";
+	html += "<div id=\"editor\">" + markdown + "</div><div id=\"preview\"></div>";
 	document.getElementsByTagName("main")[0].innerHTML = html;
+	refreshPreview(name, markdown);
 
 	var stateObject = { lessonName: name };
 	if(!noHistory)
@@ -42,6 +40,17 @@ function showLesson(name, markdown, noHistory)
 	editor.setTheme("ace/theme/dreamweaver");
 	editor.getSession().setMode("ace/mode/markdown");
 	editor.getSession().setUseWrapMode(true);
+	editor.getSession().on("change", function()
+		{
+			refreshPreview(name, editor.getValue());
+		});
+}
+
+function refreshPreview(name, markdown)
+{
+	var html = "<h1>" + name + "</h1>";
+	html += converter.makeHtml(markdown);
+	document.getElementById("preview").innerHTML = html;
 }
 
 function discard()
