6361516fe24206980c396514bac098a9db9b5951
GenaBitu <genabitu@gmail.com>
2017-05-06 12:07:44 +0200

    Better queueing of refreshes

--- admin/scripts/getLesson.js
+++ admin/scripts/getLesson.js
@@ -1,16 +1,3 @@
-var converter;
-var worker;
-
-function getLessonSetup()
-{
-	if(!window.Worker)
-	{
-		converter = new showdown.Converter({extensions: ["notes"]});
-		converter.setOption("noHeaderId", "true");
-		converter.setOption("tables", "true");
-	}
-}
-
 function getLesson(lesson, noHistory)
 {
 	if(lesson === undefined || lesson === "")
@@ -29,6 +16,7 @@ function showLesson(name, markdown, noHistory)
 	var html = "<header><div id=\"discard\"><i class=\"icon-left-big\"></i>Zrušit</div><div id=\"save\">Uložit<i class=\"icon-floppy\"></i></div></header>"
 	html += "<div id=\"editor\">" + markdown + "</div><div id=\"preview\"></div>";
 	document.getElementsByTagName("main")[0].innerHTML = html;
+	refreshPreview(name, markdown);
 
 	var stateObject = { lessonName: name };
 	if(!noHistory)
@@ -43,43 +31,16 @@ function showLesson(name, markdown, noHistory)
 	editor.setTheme("ace/theme/dreamweaver");
 	editor.getSession().setMode("ace/mode/markdown");
 	editor.getSession().setUseWrapMode(true);
-	if(window.Worker)
-	{
-		worker = new Worker("scripts/previewWorker.js");
-		worker.onmessage = function(message)
-		{
-			document.getElementById("preview").innerHTML = "<h1>" + name + "</h1>" + message.data;
-		}
-	}
 	editor.getSession().on("change", function()
 		{
 			refreshPreview(name, editor.getValue());
 		});
-	refreshPreview(name, markdown);
-}
-
-function refreshPreview(name, markdown)
-{
-	if(window.Worker)
-	{
-		worker.postMessage(markdown);
-	}
-	else
-	{
-		var html = "<h1>" + name + "</h1>";
-		html += converter.makeHtml(markdown);
-		document.getElementById("preview").innerHTML = html;
-	}
 }
 
 function discard()
 {
 	if(confirm("Opravdu si přejete zahodit všechny změny?"))
 	{
-		if(window.Worker)
-		{
-			worker.terminate();
-		}
 		history.back();
 	}
 }
--- admin/scripts/main.js
+++ admin/scripts/main.js
@@ -1,6 +1,6 @@
 function main()
 {
-	getLessonSetup();
+	refreshPreviewSetup();
 	historySetup();
 	mainPageSetup();
 }
