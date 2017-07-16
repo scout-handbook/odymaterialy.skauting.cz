344bd51737bc4df85c60c67c0481758f5cd069b9
GenaBitu <genabitu@gmail.com>
2017-07-16 18:25:55 +0200

    Anonymous lessons in mainpage(frontend and admin)

--- admin/scripts/mainPage.js
+++ admin/scripts/mainPage.js
@@ -65,26 +65,53 @@ function renderLessonList()
 	var html = "";
 	for(var i = 0; i < FIELDS.length; i++)
 	{
-		html += "<h2 class=\"mainPage\">" + FIELDS[i].name + "</h2>";
-		for(var j = 0; j < FIELDS[i].lessons.length; j++)
+		if(FIELDS[i].name)
 		{
-			html += "<h3 class=\"mainPage\"><a title=\"" + FIELDS[i].lessons[j].name + "\" href=\"/error/enableJS.html\" data-id=\"" + FIELDS[i].lessons[j].id + "\">" + FIELDS[i].lessons[j].name + "</a></h3>";
-			if(FIELDS[i].lessons[j].competences.length > 0)
+			html += "<h2 class=\"mainPage\">" + FIELDS[i].name + "</h2>";
+			for(var j = 0; j < FIELDS[i].lessons.length; j++)
 			{
-				var competences = [];
-				for(var k = 0; k < COMPETENCES.length; k++)
+				html += "<h3 class=\"mainPage secondLevel\"><a title=\"" + FIELDS[i].lessons[j].name + "\" href=\"/error/enableJS.html\" data-id=\"" + FIELDS[i].lessons[j].id + "\">" + FIELDS[i].lessons[j].name + "</a></h3>";
+				if(FIELDS[i].lessons[j].competences.length > 0)
 				{
-					if(FIELDS[i].lessons[j].competences.indexOf(COMPETENCES[k].id) >= 0)
+					var competences = [];
+					for(var k = 0; k < COMPETENCES.length; k++)
 					{
-						competences.push(COMPETENCES[k]);
+						if(FIELDS[i].lessons[j].competences.indexOf(COMPETENCES[k].id) >= 0)
+						{
+							competences.push(COMPETENCES[k]);
+						}
 					}
+					html += "<span class=\"mainPage secondLevel\">Kompetence: " + competences[0].number;
+					for(var m = 1; m < competences.length; m++)
+					{
+						html += ", " + competences[m].number;
+					}
+					html += "</span>";
 				}
-				html += "<span class=\"mainPage\">Kompetence: " + competences[0].number;
-				for(var m = 1; m < competences.length; m++)
+			}
+		}
+		else
+		{
+			for(var j = 0; j < FIELDS[i].lessons.length; j++)
+			{
+				html += "<h3 class=\"mainPage\"><a title=\"" + FIELDS[i].lessons[j].name + "\" href=\"/error/enableJS.html\" data-id=\"" + FIELDS[i].lessons[j].id + "\">" + FIELDS[i].lessons[j].name + "</a></h3>";
+				if(FIELDS[i].lessons[j].competences.length > 0)
 				{
-					html += ", " + competences[m].number;
+					var competences = [];
+					for(var k = 0; k < COMPETENCES.length; k++)
+					{
+						if(FIELDS[i].lessons[j].competences.indexOf(COMPETENCES[k].id) >= 0)
+						{
+							competences.push(COMPETENCES[k]);
+						}
+					}
+					html += "<span class=\"mainPage\">Kompetence: " + competences[0].number;
+					for(var m = 1; m < competences.length; m++)
+					{
+						html += ", " + competences[m].number;
+					}
+					html += "</span>";
 				}
-				html += "</span>";
 			}
 		}
 	}
