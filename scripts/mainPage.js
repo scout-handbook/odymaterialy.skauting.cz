0c3aaf55eec64284180096d9e7ccc1ba0bf57609
GenaBitu <genabitu@gmail.com>
2017-07-26 22:12:23 +0200

    Simplified mainPage rendering

--- scripts/mainPage.js
+++ scripts/mainPage.js
@@ -36,55 +36,33 @@ function showMainPage(noHistory)
 function renderLessonList()
 {
 	var html = "";
+	var secondLevel = "";
 	for(var i = 0; i < FIELDS.length; i++)
 	{
 		if(FIELDS[i].name)
 		{
 			html += "<h2 class=\"mainPage\"><a title=\"" + FIELDS[i].name + "\" href=\"/error/enableJS.html\" data-id=\"" + FIELDS[i].id + "\">" + FIELDS[i].name + "</a></h2>";
-			for(var j = 0; j < FIELDS[i].lessons.length; j++)
-			{
-				html += "<h3 class=\"mainPage secondLevel\"><a title=\"" + FIELDS[i].lessons[j].name + "\" href=\"/error/enableJS.html\" data-id=\"" + FIELDS[i].lessons[j].id + "\">" + FIELDS[i].lessons[j].name + "</a></h3>";
-				if(FIELDS[i].lessons[j].competences.length > 0)
-				{
-					var competences = [];
-					for(var k = 0; k < COMPETENCES.length; k++)
-					{
-						if(FIELDS[i].lessons[j].competences.indexOf(COMPETENCES[k].id) >= 0)
-						{
-							competences.push(COMPETENCES[k]);
-						}
-					}
-					html += "<span class=\"mainPage secondLevel\">Kompetence: " + competences[0].number;
-					for(var m = 1; m < competences.length; m++)
-					{
-						html += ", " + competences[m].number;
-					}
-					html += "</span>";
-				}
-			}
+			secondLevel = " secondLevel";
 		}
-		else
+		for(var j = 0; j < FIELDS[i].lessons.length; j++)
 		{
-			for(var j = 0; j < FIELDS[i].lessons.length; j++)
+			html += "<h3 class=\"mainPage" + secondLevel + "\"><a title=\"" + FIELDS[i].lessons[j].name + "\" href=\"/error/enableJS.html\" data-id=\"" + FIELDS[i].lessons[j].id + "\">" + FIELDS[i].lessons[j].name + "</a></h3>";
+			if(FIELDS[i].lessons[j].competences.length > 0)
 			{
-				html += "<h3 class=\"mainPage\"><a title=\"" + FIELDS[i].lessons[j].name + "\" href=\"/error/enableJS.html\" data-id=\"" + FIELDS[i].lessons[j].id + "\">" + FIELDS[i].lessons[j].name + "</a></h3>";
-				if(FIELDS[i].lessons[j].competences.length > 0)
+				var competences = [];
+				for(var k = 0; k < COMPETENCES.length; k++)
 				{
-					var competences = [];
-					for(var k = 0; k < COMPETENCES.length; k++)
+					if(FIELDS[i].lessons[j].competences.indexOf(COMPETENCES[k].id) >= 0)
 					{
-						if(FIELDS[i].lessons[j].competences.indexOf(COMPETENCES[k].id) >= 0)
-						{
-							competences.push(COMPETENCES[k]);
-						}
+						competences.push(COMPETENCES[k]);
 					}
-					html += "<span class=\"mainPage\">Kompetence: " + competences[0].number;
-					for(var m = 1; m < competences.length; m++)
-					{
-						html += ", " + competences[m].number;
-					}
-					html += "</span>";
 				}
+				html += "<span class=\"mainPage" + secondLevel + "\">Kompetence: " + competences[0].number;
+				for(var m = 1; m < competences.length; m++)
+				{
+					html += ", " + competences[m].number;
+				}
+				html += "</span>";
 			}
 		}
 	}
