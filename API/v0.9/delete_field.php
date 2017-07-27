a556bdb22525f3c18f67ffee523e2bfcbf60a42d
GenaBitu <genabitu@gmail.com>
2017-07-28 00:28:50 +0200

    Added delete_field API endpoint

---
+++ API/v0.9/delete_field.php
@@ -0,0 +1,71 @@
+<?php
+const _API_EXEC = 1;
+
+header('content-type:application/json; charset=utf-8');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/skautisTry.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/database.secret.php');
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/APIException.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/ArgumentException.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/ConnectionException.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/ExecutionException.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/QueryException.php');
+
+function delete()
+{
+	if(!isset($_POST['id']))
+	{
+		throw new OdyMaterialyAPI\ArgumentException(OdyMaterialyAPI\ArgumentException::POST, 'id');
+	}
+	$id = $_POST['id'];
+
+	$db = new mysqli(OdyMaterialyAPI\DB_SERVER, OdyMaterialyAPI\DB_USER, OdyMaterialyAPI\DB_PASSWORD, OdyMaterialyAPI\DB_DBNAME);
+	if ($db->connect_error)
+	{
+		throw new OdyMaterialyAPI\ConnectionException($db);
+	}
+
+	$deleteLessonsSQL = <<<SQL
+DELETE FROM lessons_in_fields
+WHERE field_id = ?;
+SQL;
+	$deleteSQL = <<<SQL
+DELETE FROM fields
+WHERE id = ?;
+SQL;
+
+	$deleteLessonsStatement = $db->prepare($deleteLessonsSQL);
+	if(!$deleteLessonsStatement)
+	{
+		throw new OdyMaterialyAPI\QueryException($deleteLessonsSQL, $db);
+	}
+	$deleteLessonsStatement->bind_param('i', $id);
+	if(!$deleteLessonsStatement->execute())
+	{
+		throw new OdyMaterialyAPI\ExecutionException($deleteLessonsSQL, $deleteLessonsStatement);
+	}
+	$deleteLessonsStatement->close();
+
+	$deleteStatement = $db->prepare($deleteSQL);
+	if(!$deleteStatement)
+	{
+		throw new OdyMaterialyAPI\QueryException($deleteSQL, $db);
+	}
+	$deleteStatement->bind_param('i', $id);
+	if(!$deleteStatement->execute())
+	{
+		throw new OdyMaterialyAPI\ExecutionException($deleteSQL, $deleteStatement);
+	}
+	$deleteStatement->close();
+	$db->close();
+}
+
+try
+{
+	OdyMaterialyAPI\administratorTry('delete', true);
+	echo(json_encode(array('success' => true)));
+}
+catch(OdyMaterialyAPI\APIException $e)
+{
+	echo($e);
+}
