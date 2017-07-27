3737454c041e42b4fa3ea0e5cfafd5c253bd66a4
GenaBitu <genabitu@gmail.com>
2017-07-27 13:18:38 +0200

    Added deleteLesson to admin

---
+++ admin/scripts/deleteLesson.js
@@ -0,0 +1,27 @@
+function deleteLessonOnClick(event)
+{
+	var name = "";
+	outer:
+	for(var i = 0; i < FIELDS.length; i++)
+	{
+		for(var j = 0; j < FIELDS[i].lessons.length; j++)
+		{
+			if(FIELDS[i].lessons[j].id == event.target.dataset.id)
+			{
+				name = FIELDS[i].lessons[j].name
+				break outer;
+			}
+		}
+	}
+
+	var stateObject = { "sidePanel": "open" };
+	history.pushState(stateObject, "title", "/admin/");
+
+	dialog("Opravdu si přejete smazat lekci \"" + FIELDS[i].lessons[j].name + "\"?", "Ano", function()
+		{
+			retryAction("/API/v0.9/delete_lesson", "id=" + event.target.dataset.id);
+		}, "&nbsp;&nbsp;Ne&nbsp;&nbsp;", function()
+		{
+			history.back();
+		});
+}
--- admin/scripts/mainPage.js
+++ admin/scripts/mainPage.js
@@ -58,7 +58,11 @@ function showMainPage(noHistory)
 	{
 		nodes[l].onclick = changeCompetencesOnClick;
 	}
-
+	nodes = document.getElementsByTagName("main")[0].getElementsByClassName("deleteLesson");
+	for(var l = 0; l < nodes.length; l++)
+	{
+		nodes[l].onclick = deleteLessonOnClick;
+	}
 
 	document.getElementsByTagName("main")[0].scrollTop = 0;
 	var stateObject = { lessonName: "" };
@@ -107,6 +111,7 @@ function renderLessonList()
 			}
 			html += "<div class=\"button mainPage" + secondLevel + " changeField\" data-id=\"" + FIELDS[i].lessons[j].id + "\">Změnit oblast</div>";
 			html += "<div class=\"button mainPage changeCompetences\" data-id=\"" + FIELDS[i].lessons[j].id + "\">Změnit kompetence</div>";
+			html += "<div class=\"button mainPage deleteLesson\" data-id=\"" + FIELDS[i].lessons[j].id + "\">Smazat lekci</div>";
 		}
 	}
 	return html;
