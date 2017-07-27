87d44d4685ad1a8f65c32c2f8cb92bfffc6c088b
GenaBitu <genabitu@gmail.com>
2017-07-28 00:09:15 +0200

    Added add_field API endpoint

---
+++ API/v0.9/add_field.php
@@ -0,0 +1,56 @@
+<?php
+const _API_EXEC = 1;
+
+header('content-type:application/json; charset=utf-8');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/skautisTry.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/database.secret.php');
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/APIException.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/ArgumentException.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/ConnectionException.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/ExecutionException.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/API/internal/QueryException.php');
+
+function updateField()
+{
+	if(!isset($_POST['name']))
+	{
+		throw new OdyMaterialyAPI\ArgumentException(OdyMaterialyAPI\ArgumentException::POST, 'name');
+	}
+	$name = $_POST['name'];
+
+	$db = new mysqli(OdyMaterialyAPI\DB_SERVER, OdyMaterialyAPI\DB_USER, OdyMaterialyAPI\DB_PASSWORD, OdyMaterialyAPI\DB_DBNAME);
+
+	if ($db->connect_error)
+	{
+		throw new OdyMaterialyAPI\ConnectionException($db);
+	}
+
+	$SQL = <<<SQL
+INSERT INTO fields (name)
+VALUES (?);
+SQL;
+
+	$statement = $db->prepare($SQL);
+	if(!$statement)
+	{
+		throw new OdyMaterialyAPI\QueryException($SQL, $db);
+	}
+	$statement->bind_param('s', $name);
+	if(!$statement->execute())
+	{
+		throw new OdyMaterialyAPI\ExecutionException($SQL, $statement);
+	}
+	$statement->close();
+	$db->close();
+}
+
+try
+{
+	OdyMaterialyAPI\administratorTry('updateField', true);
+	echo(json_encode(array('success' => true)));
+}
+catch(OdyMaterialyAPI\APIException $e)
+{
+	echo($e);
+}
