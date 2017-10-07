050f3197444a29fe2413eed2bee279c61714a324
GenaBitu <genabitu@gmail.com>
2017-10-07 16:54:15 +0200

    Travis test 6

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/swagger-api/swagger-ui.git
 REPL=$(echo "    urls: [$(ls API | awk -v ORS= '{print "{url: \"https://raw.githubusercontent.com/GenaBitu/OdyMaterialy/master/API/" $1 "/swagger.yaml\", name: \"Version " substr($1, 2) "\"}, "}' | sed 's/..$//')],")
 cd swagger-ui
+npm install
 sed -i -e "/url: \"http:\/\/petstore\.swagger\.io\/v2\/swagger\.json\",/c \\${REPL}" dist/index.html
 npm run build
