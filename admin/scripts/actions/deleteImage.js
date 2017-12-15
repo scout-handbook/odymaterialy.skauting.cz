fe36833c119fb8523834dd82a15f2fd2300dc85b
GenaBitu <genabitu@gmail.com>
2017-12-15 01:43:12 +0100

    Restyled image prompts

--- admin/scripts/actions/deleteImage.js
+++ admin/scripts/actions/deleteImage.js
@@ -3,7 +3,7 @@ function deleteImageOnClick(event)
 	dialog("Opravdu si přejete smazat tento obrázek?", "Ano", function()
 		{
 			spinner();
-			retryAction("/API/v0.9/image/" + encodeURIComponent(event.target.dataset.id), "DELETE", {});
+			retryAction("/API/v0.9/image/" + encodeURIComponent(getAttribute(event, "id")), "DELETE", {});
 		}, "Ne", function(){history.back();});
 	history.pushState({"sidePanel": "open"}, "title", "/admin/images");
 	refreshLogin();
