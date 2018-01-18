58dcdd405cc5580aeb7e7ac4e6129db9aee3a204
GenaBitu <genabitu@gmail.com>
2018-01-18 01:02:48 +0100

    Updated default lesson content

--- admin/scripts/lessonEditor/defaultContent.js
+++ admin/scripts/lessonEditor/defaultContent.js
@@ -29,25 +29,17 @@ začít nový odstavec, musím vynechat jeden řádek. Můžu udělat kus textu 
         - hodně\n\
         - hluboko\n\
 \n\
-Můžu do textu vložit [odkaz](http://tiny.cc/PAIN), i když ten nevím, jak bych ho vytisknul :D. Fungují i obrázky:\n\
+Můžu do textu vložit [odkaz](http://tiny.cc/PAIN), při tisku se adresa dá do závorky za textem. Fungují i obrázky:\n\
 \n\
 ![Text po najetí kurzorem](https://odymaterialy.skauting.cz/API/v0.9/image/ed02b61f-ef4c-40e8-9018-3acbe071316d)\n\
 \n\
-Akorát jsem ještě neudělal nahrávání vlastních (a vlastně nevím, jak je vytisknout, tak mi je asi budete muset poslat...)...\n\
-\n\
-Pak taky můzu udělat něco jako\n\
-\n\
- > citaci,\n\
- > která bude odsazená, ale nevím, jestli to je k něčemu dobré...\n\
-\n\
-\n\
 No a taky můžu dělat tabulky:\n\
 \n\
-| Tables        | Are           | Cool  |\n\
-| ------------- |:-------------:| -----:|\n\
-| col 3 is      | right-aligned | $1600 |\n\
-| col 2 is      | centered      |   $12 |\n\
-| zebra stripes | are neat      |    $1 |\n\
+| Tables            | Are                  | Cool  |\n\
+| ------------------ |:-------------------:| -------:|\n\
+| col 3 is            | right-aligned | 1600 |\n\
+| col 2 is            | centered        | 12     |\n\
+| zebra stripes | are neat         | 1       |\n\
 \n\
 Pod hlavičkou musí být řada pomlček (aspoň 3 v kadém sloupci) a můžu tam použít : k zarovnání vlevo, na střed nebo doprava.\n\
 \n\
@@ -66,8 +58,4 @@ mi udělá tři prázdné řádky, nebo můžu:\n\
 \n\
 !notes[style=dotted, height = 5]\n\
 \n\
-udělat takové ty vytečkované řádky (tady 5 řádků), nebo taky\n\
-\n\
-!notes[style=dotted,height=eop]\n\
-\n\
-tečkované řádky až do konce stránky (End Of Page), s tím malým detailem, že jak ta stránka vypadá, tu zatím nikde není vidět, to musím ještě nějak vymyslet...."
+udělat takové ty vytečkované řádky (tady 5 řádků). Na webu se to nezobrazí, ale v PDF ano."
