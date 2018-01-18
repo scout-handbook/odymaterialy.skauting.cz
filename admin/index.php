b31071c7254e0051665a074a2755e680b04d3663
GenaBitu <genabitu@gmail.com>
2018-01-18 21:28:39 +0100

    Fixed admin not redirecting to login

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,10 @@
 <?php declare(strict_types=1);
 
-$context = ['http' => ['header'=>'Cookie: skautis_timeout=' . $_COOKIE['skautis_timeout'] . '; skautis_token=' . $_COOKIE['skautis_token']]];
+$context = ['http' => ['ignore_errors' => true]];
+if(isset($_COOKIE['skautis_timeout']) and isset($_COOKIE['skautis_token']))
+{
+	$context = ['http' => ['ignore_errors' => true, 'header' => 'Cookie: skautis_timeout=' . $_COOKIE['skautis_timeout'] . '; skautis_token=' . $_COOKIE['skautis_token']]];
+}
 $accountInfo = file_get_contents('https://odymaterialy.skauting.cz/API/v0.9/account?no-avatar=true', false, stream_context_create($context));
 $loginState = json_decode($accountInfo, true);
 if(isset($loginState['status']))
