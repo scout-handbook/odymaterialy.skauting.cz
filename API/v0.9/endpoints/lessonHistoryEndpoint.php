4efba8eb9c29d320200489439833b22f4fa9cb42
GenaBitu <genabitu@gmail.com>
2018-02-14 18:15:54 +0100

    Added lesson history endpoint

---
+++ API/v0.9/endpoints/lessonHistoryEndpoint.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+@_API_EXEC === 1 or die('Restricted access.');
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/settings.php');
+require_once($BASEPATH . '/vendor/autoload.php');
+require_once($BASEPATH . '/v0.9/internal/Database.php');
+require_once($BASEPATH . '/v0.9/internal/Endpoint.php');
+require_once($BASEPATH . '/v0.9/internal/Helper.php');
+require_once($BASEPATH . '/v0.9/internal/Role.php');
+
+use Ramsey\Uuid\Uuid;
+
+$lessonHistoryEndpoint = new HandbookAPI\Endpoint();
+
+$listLessonHistory = function(Skautis\Skautis $skautis, array $data, HandbookAPI\Endpoint $endpoint) : array
+{
+	$SQL = <<<SQL
+SELECT name, UNIX_TIMESTAMP(version) FROM lesson_history
+WHERE id = :id
+ORDER BY version DESC;
+SQL;
+
+	$id = HandbookAPI\Helper::parseUuid($data['parent-id'], 'lesson')->getBytes();
+
+	$db = new HandbookAPI\Database();
+	$db->prepare($SQL);
+	$db->bindParam(':id', $id, PDO::PARAM_STR);
+	$db->execute();
+	$versions = [];
+	$name = '';
+	$version = '';
+	$db->bindColumn('name', $name);
+	$db->bindColumn(2, $version);
+	while($db->fetch())
+	{
+		$versions[] = ['name' => $name, 'version' => round($version * 1000)];
+	}
+	return ['status' => 200, 'response' => $versions];
+};
+$lessonHistoryEndpoint->setListMethod(new HandbookAPI\Role('editor'), $listLessonHistory);
