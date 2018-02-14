50c1873558ad0b56af4231fb5ff38c26ca39392d
GenaBitu <genabitu@gmail.com>
2018-02-14 20:46:03 +0100

    Added lesson history version endpoint

--- API/v0.9/endpoints/lessonHistoryEndpoint.php
+++ API/v0.9/endpoints/lessonHistoryEndpoint.php
@@ -8,6 +8,8 @@ require_once($BASEPATH . '/v0.9/internal/Endpoint.php');
 require_once($BASEPATH . '/v0.9/internal/Helper.php');
 require_once($BASEPATH . '/v0.9/internal/Role.php');
 
+require_once($BASEPATH . '/v0.9/internal/exceptions/InvalidArgumentTypeException.php');
+
 use Ramsey\Uuid\Uuid;
 
 $lessonHistoryEndpoint = new HandbookAPI\Endpoint();
@@ -38,3 +40,31 @@ SQL;
 	return ['status' => 200, 'response' => $versions];
 };
 $lessonHistoryEndpoint->setListMethod(new HandbookAPI\Role('editor'), $listLessonHistory);
+
+$getLessonHistory = function(Skautis\Skautis $skautis, array $data, HandbookAPI\Endpoint $endpoint) : array
+{
+	$SQL = <<<SQL
+SELECT body
+FROM lesson_history
+WHERE id = :id
+AND version = FROM_UNIXTIME(:version);
+SQL;
+
+	$id = HandbookAPI\Helper::parseUuid($data['parent-id'], 'lesson')->getBytes();
+	$version = ctype_digit($data['id']) ? intval($data['id']) / 1000 : null;
+	if($version === null)
+	{
+		throw new HandbookAPI\InvalidArgumentTypeException('number', ['Integer']);
+	}
+
+	$db = new HandbookAPI\Database();
+	$db->prepare($SQL);
+	$db->bindParam(':id', $id, PDO::PARAM_STR);
+	$db->bindParam(':version', $version, PDO::PARAM_INT);
+	$db->execute();
+	$body = '';
+	$db->bindColumn('body', $body);
+	$db->fetchRequire('lesson');
+	return ['status' => 200, 'response' => $body];
+};
+$lessonHistoryEndpoint->setGetMethod(new HandbookAPI\Role('editor'), $getLessonHistory);
