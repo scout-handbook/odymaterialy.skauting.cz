58d5c4b1b8b13052d65919bd6c7fd562734205f8
GenaBitu <genabitu@gmail.com>
2018-02-18 00:24:44 +0100

    Added double open to sidePanel

--- admin/scripts/UI/sidePanel.js
+++ admin/scripts/UI/sidePanel.js
@@ -5,15 +5,22 @@ function sidePanelOpen()
 	var sidePanel = document.getElementById("sidePanel");
 	var overlay = document.getElementById("sidePanelOverlay");
 	sidePanel.style.right = "0";
+	sidePanel.style.width = "";
 	overlay.style.display = "inline";
 	sidePanelState = true;
+}
+
+function sidePanelDoubleOpen()
+{
+	var sidePanel = document.getElementById("sidePanel");
+	sidePanel.style.width = "1000px";
 }
 
 function sidePanelClose()
 {
 	var sidePanel = document.getElementById("sidePanel");
 	var overlay = document.getElementById("sidePanelOverlay");
-	sidePanel.style.right = "-600px";
-	overlay.style.display = "none";
+	sidePanel.style.right = "";
+	overlay.style.display = "";
 	sidePanelState = false;
 }
