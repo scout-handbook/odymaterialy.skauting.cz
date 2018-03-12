d891bba6c2ca42a8288482ff3f1c7afd32d1d942
GenaBitu <genabitu@gmail.com>
2018-03-12 12:43:10 +0100

    doiuse

---
+++ doiuse.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+ doiuse --browsers "> 1%, > 1% in CZ, IE >= 6, last 5 Firefox versions, last 5 FirefoxAndroid versions, last 5 Chrome versions, last 5 ChromeAndroid versions, last 5 iOS versions, last 5 Opera versions, last 5 Edge versions, last 5 Safari versions" styles/*
+ doiuse --browsers "> 1%, > 1% in CZ, IE >= 6, last 5 Firefox versions, last 5 FirefoxAndroid versions, last 5 Chrome versions, last 5 ChromeAndroid versions, last 5 iOS versions, last 5 Opera versions, last 5 Edge versions, last 5 Safari versions" admin/styles/*
