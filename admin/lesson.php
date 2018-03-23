41b972899921060ef9c5d8a672c8ad68096f7da5
GenaBitu <genabitu@gmail.com>
2018-03-23 14:26:42 +0100

    Admin PDF login redirect

---
+++ admin/lesson.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types = 1);
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/settings.php');
+
+$context = stream_context_get_default();
+stream_context_set_option($context, ['http' => ['ignore_errors' => true]]);
+if(isset($_COOKIE['skautis_timeout']) and isset($_COOKIE['skautis_token']))
+{
+	stream_context_set_option($context, ['http' => ['header' => 'Cookie: skautis_timeout=' . $_COOKIE['skautis_timeout'] . '; skautis_token=' . $_COOKIE['skautis_token']]]);
+}
+$accountInfo = file_get_contents($APIURI . '/account?no-avatar=true', false, $context);
+$loginState = json_decode($accountInfo, true);
+
+if(isset($loginState['status']))
+{
+	if($loginState['status'] == 200)
+	{
+		if(isset($loginState['response']['role']))
+		{
+			$role = $loginState['response']['role'];
+			if($role == 'editor' or $role == 'administrator' or $role == 'superuser')
+			{
+				header('content-type:application/pdf; charset=utf-8');
+				fpassthru(fopen($APIURI . '/lesson/' . $_GET['id'] . '/pdf', 'rb'));
+				die();
+			}
+		}
+	}
+	elseif($loginState['status'] == 401)
+	{
+		header('Location: ' . $APIURI . '/login?return-uri=' . urlencode($_SERVER['REQUEST_URI']));
+		die();
+	}
+}
+header('Location: ' . $BASEURI);
+die();
