5c3b14902c5ad3ec1d2407676147c9be6ae1ae3a
GenaBitu <genabitu@gmail.com>
2018-03-30 22:26:50 +0200

    Removed API directory

--- API/v0.9/account.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types = 1);
-const _API_EXEC = 1;
-
-require_once($_SERVER['DOCUMENT_ROOT'] . '/api-config.php');
-require_once($CONFIG->basepath . '/v0.9/endpoints/accountEndpoint.php');
-
-$accountEndpoint->handle();
