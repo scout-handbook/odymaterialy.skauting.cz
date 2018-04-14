5023763f4fc6ed35dcdbc2503d821f368077ec87
GenaBitu <genabitu@gmail.com>
2018-04-14 20:31:50 +0200

    Lesson editing: using mutexes

--- admin/scripts/views/editLesson.js
+++ admin/scripts/views/editLesson.js
@@ -5,7 +5,30 @@ var imageSelectorOpen = false;
 function showLessonEditView(id, noHistory)
 {
 	spinner();
-	request(CONFIG.apiuri + "/lesson/" + id, "GET", "", function(response)
+	request(CONFIG.apiuri + "/mutex/" + encodeURIComponent(id), "POST", {}, function(response)
+		{
+			if(response.status === 201)
+			{
+				getLessonEditView(id, noHistory);
+			}
+			else if(response.status === 409)
+			{
+				dialog("Nelze upravovat lekci, protože ji právě upravuje " + response.response.holder + ".", "OK");
+			}
+			else if(response.type === "AuthenticationException")
+			{
+				window.location.replace(CONFIG.apiuri + "/login");
+			}
+			else
+			{
+				dialog("Nastala neznámá chyba. Chybová hláška:<br>" + response.message, "OK");
+			}
+		});
+}
+
+function getLessonEditView(id, noHistory)
+{
+	request(CONFIG.apiuri + "/lesson/" + encodeURIComponent(id), "GET", {}, function(response)
 		{
 			if(response.status === 200)
 			{
@@ -34,8 +57,11 @@ function renderLessonEditView(id, markdown, noHistory)
 		history.pushState({"id": id}, "title", "/admin/lessons");
 	}
 
-	var aq = new ActionQueue([new Action(CONFIG.apiuri + "/lesson/" + encodeURIComponent(id) , "PUT", saveLessonPayloadBuilder)]);
-	showLessonEditor(lesson.name, markdown, aq, id);
+	var saveActionQueue = new ActionQueue([
+		new Action(CONFIG.apiuri + "/lesson/" + encodeURIComponent(id), "PUT", saveLessonPayloadBuilder),
+		new Action(CONFIG.apiuri + "/mutex/" + encodeURIComponent(id), "DELETE")]);
+	var discardActionQueue = new ActionQueue([new Action(CONFIG.apiuri + "/mutex/" + encodeURIComponent(id), "DELETE")]);
+	showLessonEditor(lesson.name, markdown, saveActionQueue, id, discardActionQueue);
 	document.getElementById("save").dataset.id = id;
 }
 
