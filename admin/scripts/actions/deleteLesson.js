ac78197d91fa333bab7e5d2747e0b7c12d960f29
GenaBitu <genabitu@gmail.com>
2018-04-16 10:15:50 +0200

    Lesson locking on delete

--- admin/scripts/actions/deleteLesson.js
+++ admin/scripts/actions/deleteLesson.js
@@ -2,8 +2,31 @@
 
 function deleteLessonOnClick(event)
 {
-	var name = "";
 	var id = getAttribute(event, "id");
+	spinner();
+	request(CONFIG.apiuri + "/mutex/" + encodeURIComponent(id), "POST", {}, function(response)
+		{
+			if(response.status === 201)
+			{
+				deleteLessonDialog(id);
+			}
+			else if(response.status === 409)
+			{
+				dialog("Nelze smazat lekci, protože ji právě upravuje " + response.response.holder + ".", "OK");
+			}
+			else if(response.type === "AuthenticationException")
+			{
+				window.location.replace(CONFIG.apiuri + "/login");
+			}
+			else
+			{
+				dialog("Nastala neznámá chyba. Chybová hláška:<br>" + response.message, "OK");
+			}
+		});
+}
+function deleteLessonDialog(id)
+{
+	var name = "";
 	outer:
 	for(var i = 0; i < FIELDS.length; i++)
 	{
@@ -17,8 +40,15 @@ function deleteLessonOnClick(event)
 		}
 	}
 
-	var aq = new ActionQueue([new Action(CONFIG.apiuri + "/lesson/" + encodeURIComponent(id), "DELETE")]);
-	dialog("Opravdu si přejete smazat lekci \"" + name + "\"?", "Ano", aq.closeDispatch, "Ne", function(){history.back();});
+	var saveExceptionHandler = {"NotLockedException": function(){dialog("Kvůli příliš malé aktivitě byla lekce odemknuta a již ji upravil někdo jiný. Zkuste to prosím znovu.", "OK");}};
+	var discardExceptionHandler = {"NotFoundException": function(){}};
+	var saveActionQueue = new ActionQueue([new Action(CONFIG.apiuri + "/lesson/" + encodeURIComponent(id), "DELETE", function() {}, function() {}, saveExceptionHandler)]);
+	var discardActionQueue = new ActionQueue([new Action(CONFIG.apiuri + "/mutex/" + encodeURIComponent(id) , "DELETE", function(){}, function(){}, discardExceptionHandler)]);
+	dialog("Opravdu si přejete smazat lekci \"" + name + "\"?", "Ano", saveActionQueue.closeDispatch, "Ne", function()
+		{
+			discardActionQueue.dispatch(true);
+			history.back();
+		});
 	history.pushState({"sidePanel": "open"}, "title", "/admin/lessons");
 	refreshLogin();
 }
