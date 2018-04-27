ad668ea2eded1aa12327c92c6cc4136669084bce
GenaBitu <genabitu@gmail.com>
2018-04-27 17:29:31 +0200

    parameterized browserlist in doiuse

--- doiuse.sh
+++ doiuse.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
- doiuse --browsers "> 1%, > 1% in CZ, IE >= 6, last 5 Firefox versions, last 5 FirefoxAndroid versions, last 5 Chrome versions, last 5 ChromeAndroid versions, last 5 iOS versions, last 5 Opera versions, last 5 Edge versions, last 5 Safari versions" styles/*
- doiuse --browsers "> 1%, > 1% in CZ, IE >= 6, last 5 Firefox versions, last 5 FirefoxAndroid versions, last 5 Chrome versions, last 5 ChromeAndroid versions, last 5 iOS versions, last 5 Opera versions, last 5 Edge versions, last 5 Safari versions" admin/styles/*
+BROWSERLIST="> 1%, > 1% in CZ, IE >= 6, last 5 Firefox versions, last 5 FirefoxAndroid versions, last 5 Chrome versions, last 5 ChromeAndroid versions, last 5 iOS versions, last 5 Opera versions, last 5 Edge versions, last 5 Safari versions"
+
+ doiuse --browsers "$BROWSERLIST" styles/*
+ doiuse --browsers "$BROWSERLIST" admin/styles/*
