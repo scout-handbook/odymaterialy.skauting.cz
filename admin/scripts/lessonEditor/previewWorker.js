b29422660edbc5f3fc27f0d3bea1758814df1051
GenaBitu <genabitu@gmail.com>
2018-05-24 11:11:57 +0200

    lessonEditor: XSS filtering in Web worker

--- admin/scripts/lessonEditor/previewWorker.js
+++ admin/scripts/lessonEditor/previewWorker.js
@@ -6,7 +6,9 @@ function main()
 {
 	self.onmessage = process;
 	importScripts('/node_modules/showdown/dist/showdown.min.js');
+	importScripts('/node_modules/xss/dist/xss.min.js');
 	importScripts('/scripts/tools/OdyMarkdown.js');
+	importScripts('/scripts/tools/xssOptions.js');
 	converter = new showdown.Converter({extensions: ["OdyMarkdown"]});
 	converter.setOption("noHeaderId", "true");
 	converter.setOption("tables", "true");
@@ -15,7 +17,7 @@ function main()
 
 function process(payload)
 {
-	var html = converter.makeHtml(payload.data.body);
+	var html = filterXSS(converter.makeHtml(payload.data.body), xssOptions());
 	postMessage({"id": payload.data.id, "body": html});
 }
 
