bc1fcbac84cccac766971d45aac011a0674465cd
GenaBitu <genabitu@gmail.com>
2018-07-30 17:35:21 +0200

    Added Feature-Policy header

--- scripts/tools/config.js
+++ scripts/tools/config.js
@@ -7,7 +7,7 @@ function configSetup()
 {
 	var configRequest = new XMLHttpRequest();
 	configRequest.overrideMimeType("application/json");
-	configRequest.open('GET', '/client-config.json');
+	configRequest.open('GET', '/client-config.json', true);
 	configRequest.onreadystatechange = function ()
 		{
 			if(this.readyState === 4 && this.status === 200)
