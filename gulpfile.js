0a2b97d1678a386e310e1bf372a4a0aa15a31ca4
GenaBitu <genabitu@gmail.com>
2018-12-08 23:30:40 +0100

    Added uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require( 'gulp' );
 var shell = require( 'gulp-shell' );
 var eslint = require('gulp-eslint');
+var uglify = require('gulp-uglify');
 
 gulp.task( 'eslint', function() {
 	return gulp.src([ '**/*.js' ])
@@ -10,3 +11,9 @@ gulp.task( 'eslint', function() {
 } );
 
 gulp.task( 'npm-check-updates', shell.task([ 'npm outdated' ], {ignoreErrors: true}) );
+
+gulp.task('uglify', function() {
+	return gulp.src(['serviceworker.js'])
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/'));
+});
