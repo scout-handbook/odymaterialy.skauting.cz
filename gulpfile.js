2a04a22f527693a6180885056a6e4fed66a87ad8
GenaBitu <genabitu@gmail.com>
2018-12-08 22:20:00 +0100

    Added eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var gulp = require( 'gulp' );
 var shell = require( 'gulp-shell' );
+var eslint = require('gulp-eslint');
+
+gulp.task( 'eslint', function() {
+	return gulp.src([ '**/*.js' ])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+} );
 
 gulp.task( 'npm-check-updates', shell.task([ 'npm outdated' ], {ignoreErrors: true}) );
