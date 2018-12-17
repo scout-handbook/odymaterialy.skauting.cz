391033f10e5f7ea35f5ffd3c9b3d2dc020b078ee
Marek Dědič <developer@dedic.eu>
2018-12-17 17:09:26 +0100

    Removed src dir

--- src/admin/js/tools/parseVersion.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/* exported parseVersion */
-
-function parseVersion(version)
-{
-	var d = new Date(version);
-	return d.getDate() + ". " + (d.getMonth() + 1) + ". " + d.getFullYear() + " " + d.getHours() + ":" + ("0" + d.getMinutes()).slice(-2) + ":" + ("0" + d.getSeconds()).slice(-2);
-}
