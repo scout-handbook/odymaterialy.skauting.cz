75ce3750ce292e48e923146838b54f028f5d557a
Marek Dědič <developer@dedic.eu>
2023-03-20 12:13:58 +0100

    Added API redirects

--- src/api-config.php
+++ src/api-config.php
@@ -3,7 +3,7 @@
 @_API_EXEC === 1 or die('Restricted access.');
 
 $CONFIG = (object)[
-		'basepath' => $_SERVER['DOCUMENT_ROOT'] . '/API',
+		'basepath' => $_SERVER['DOCUMENT_ROOT'] . '/API/legacy',
 		'imagepath' => $_SERVER['DOCUMENT_ROOT'] . '/images',
 		'cookieuri' => 'odymaterialy.skauting.cz',
 		'baseuri' => 'https://odymaterialy.skauting.cz',
